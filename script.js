51806243bb8f890fded360aec203e7b08671ad9c
Carla Vidales <111818458+carlavid@users.noreply.github.com>
2023-07-25 11:09:37 -0700

    Add function that plays a single round of rps

--- script.js
+++ script.js
@@ -7,3 +7,41 @@ function getComputerChoice() {
     return choices[Math.floor(Math.random() * choices.length)];
 
 }
+
+// write function that plays a single round of rps
+// two parameters - playerSelection & computerSelection 
+// return string that declares winner of the round "You Lose! Paper beats Rock"
+function playRound(playerSelection, computerSelection) {
+    // make function's parameter case-insensitive 
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = computerSelection.toLowerCase();
+
+    // tie 
+    if (playerSelection === computerSelection) {
+        return "It's a tie!";
+    // playerSelection
+    } else if (playerSelection === "rock") {
+        if (computerSelection === "scissors") {
+            return "You Win! Rock beats Scissors";
+        } else if (computerSelection === "paper") {
+            return "You Lose! Paper beats Rock";
+        }
+    // playerSelection = paper
+    } else if (playerSelection === "paper") {
+        if (computerSelection === "rock") {
+            return "You Win! Paper beats Rock";
+        } else if (computerSelection === "scissors") {
+            return "You Lose! Scissors beats Paper";
+        }
+    // playerSelection = scissors
+    } else if (playerSelection === "scissors") {
+        if (computerSelection === "paper") {
+            return "You Win! Scissors beats Paper";
+        } else if (computerSelection === "rock") {
+            return "You Lose! Rock beats Scissors"
+        }
+    }
+}
+
+
+
