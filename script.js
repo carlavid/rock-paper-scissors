14c7fe4012cd4a648968ef1d160a6eb1075db758
Carla Vidales <111818458+carlavid@users.noreply.github.com>
2023-08-01 20:26:13 -0700

    Add UI

--- script.js
+++ script.js
@@ -1,99 +1,102 @@
 let playerScore = 0;
 let computerScore = 0;
 
+const playerScore_span = document.getElementById("player-score");
+const computerScore_span = document.getElementById("computer-score");
+const roundResults = document.getElementById("results");
+const rockButton = document.getElementById("rock");
+const paperButton = document.getElementById("paper");
+const scissorsButton = document.getElementById("scissors");
+const buttons = document.querySelectorAll("button");
+
 
 function getComputerChoice() {
     const choices = ["rock", "paper", "scissors"]
     return choices[Math.floor(Math.random() * choices.length)];
-
 }
 
 function playRound(playerSelection, computerSelection) {
     let results = "";
     // tie 
     if (playerSelection === computerSelection) {
-        playerScore += 1;
-        computerScore +=1;
+        playerScore++;
+        computerScore++;
         results = "It's a tie!";
     // playerSelection
     } else if (playerSelection === "rock") {
         if (computerSelection === "scissors") {
-            playerScore += 1;
+            playerScore++;
             results = "You Win! Rock beats Scissors";
         } else if (computerSelection === "paper") {
-            computerScore += 1;
+            computerScore++;
             results = "You Lose! Paper beats Rock";
         }
     // playerSelection = paper
     } else if (playerSelection === "paper") {
         if (computerSelection === "rock") {
-            playerScore += 1;
+            playerScore++;
             results = "You Win! Paper beats Rock";
         } else if (computerSelection === "scissors") {
-            computerScore += 1;
+            computerScore++;
             results = "You Lose! Scissors beats Paper";
         }
     // playerSelection = scissors
     } else if (playerSelection === "scissors") {
         if (computerSelection === "paper") {
-            playerScore += 1;
+            playerScore++;
             results = "You Win! Scissors beats Paper";
         } else if (computerSelection === "rock") {
-            computerScore += 1;
+            computerScore++;
             results =  "You Lose! Rock beats Scissors"
         }
     }
-    return `<p><strong>${results}</strong></p>
-    <br>Player Score: <strong>${playerScore}</strong>
-    <br>Computer Score: <strong>${computerScore}</strong>`;
+    roundResults.innerHTML = `<p><strong>${results}</strong></p>`;
+    playerScore_span.innerHTML = `Player Score: <strong>${playerScore}</strong> `;
+    computerScore_span.innerHTML = `Computer Score: <strong>${computerScore}</strong>`;
+    return roundResults.innerHTML + playerScore_span.innerHTML + computerScore_span.innerHTML;
 };
-
-
 
 function game() {
     let computerSelection = getComputerChoice();
     let finalResults = playRound(playerSelection, computerSelection);
     
-    // return finalResults;
     return finalResults;
     
 }
 
-
-const rockButton = document.querySelector(".rock");
+// add event listeners to each button
 rockButton.addEventListener("click", function() {
     playerSelection = "rock";
     document.getElementById("div1").innerHTML = game();
 })
 
-
-const paperButton = document.querySelector(".paper");
 paperButton.addEventListener("click", function() {
     playerSelection = "paper";
     document.getElementById("div1").innerHTML = game();
 })
 
-const scissorsButton = document.querySelector(".scissors");
 scissorsButton.addEventListener("click", function() {
     playerSelection = "scissors";
     document.getElementById("div1").innerHTML = game();
 })
 
-// disable buttons after 5 rounds;
-const buttons = document.querySelectorAll("button");
-let clickCount = 0;
-const maxClicks = 5;
-
+// disable buttons after player or computer reaches 5;
 function clickHandler() {
-    clickCount++;
-
-    if (clickCount > maxClicks) {
+    if (playerScore === 5 || computerScore === 5) {
         buttons.forEach(button => {
             button.disabled = true;
         });
-        document.getElementById("div1").innerHTML = "Game Over! Reload to play again!"
+        if (playerScore > computerScore) {
+            document.getElementById("final-winner").innerHTML = "You won the game!🥳 Reload to play again!";
+        } else if (computerScore > playerScore) {
+            document.getElementById("final-winner").innerHTML = "You lost the game!😞 Reload to play again!";
+        } else if (computerScore === playerScore) {
+            document.getElementById("final-winner").innerHTML = "It's a tie! Reload to play again!";
+        }
+        
     }
 }
+
 rockButton.addEventListener("click", clickHandler);
 paperButton.addEventListener("click", clickHandler);
 scissorsButton.addEventListener("click", clickHandler);
